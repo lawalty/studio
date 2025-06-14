48095bb650669415ebd960f9d49404c59f977471
Lloyd Walty <lawalty@gmail.com>
2025-06-14 10:26:02 +0000

    See the error.

---
+++ functions/.eslintrc.js
@@ -0,0 +1,41 @@
+
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'google',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'], // Path relative to this .eslintrc.js file
+    sourceType: 'module',
+    ecmaVersion: 2021, // Or your target ECMAScript version
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  ignorePatterns: [
+    '/lib/**/*', // Built files
+    '/node_modules/**/*',
+    '.eslintrc.js', // Ignore this file itself
+  ],
+  rules: {
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    'indent': ['error', 2],
+    'object-curly-spacing': ['error', 'always'],
+    'require-jsdoc': 'off', // Turned off for simplicity, can be re-enabled
+    'valid-jsdoc': 'off',   // Turned off for simplicity
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    'import/no-unresolved': 0, // Can be helpful with TypeScript paths
+    'max-len': ['warn', {'code': 120, 'ignoreComments': true, 'ignoreUrls': true}],
+    'operator-linebreak': ['error', 'after'],
+    'no-prototype-builtins': 'warn', // Firebase often uses this
+    'spaced-comment': ['error', 'always', { 'markers': ['/'] }], // Allow triple slash directives
+  },
+};
