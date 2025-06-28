5e1a7c434ba806d925a9ee4b90778d5f325ddb22
Lloyd Walty <lawalty@gmail.com>
2025-06-28 20:21:10 +0000

    Do me a favor, go ahead and create another simple start page that isn't

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -22,6 +23,15 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/temp-start',
+        permanent: false,
+      },
+    ];
   },
 };
 
