4e85a6f91127ab220312069048cf1053a0b683a9
Lloyd Walty <lawalty@gmail.com>
2025-07-14 15:33:09 +0000

    I gotta feeling we're close to publishing, se we can re-focus on the RAG

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,7 +2,7 @@
 'use server';
 import { type Plugin } from '@genkit-ai/core';
 import { googleAI } from '@genkit-ai/googleai';
-import { firebase as firebasePlugin } from '@genkit-ai/firebase';
+import firebasePlugin from '@genkit-ai/firebase';
 import { genkit } from 'genkit';
 
 const plugins: Plugin<any>[] = [
