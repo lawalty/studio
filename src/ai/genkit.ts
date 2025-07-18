adc4475900dc2b59afb26f44b1ae5506677499f2
Lloyd Walty <lawalty@gmail.com>
2025-07-18 14:33:45 +0000

    Linting and checking validity of types ... Failed to compile. ./src/ai/g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,5 @@
 import { googleAI } from '@genkit-ai/googleai';
-import { firebase } from '@genkit-ai/firebase';
+import firebase from '@genkit-ai/firebase';
 import { genkit, type Plugin } from 'genkit';
 
 const plugins: Plugin<any>[] = [googleAI()];
